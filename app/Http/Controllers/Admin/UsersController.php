1e6dae40962d5dbdf1cb09eb503a6d408de15ebc
rayhanhossen <rayhanhossen.cse@gmail.com>
2020-09-17 02:13:34 +0600

    create role

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -15,7 +15,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        return view('admin.user.index');
+        $users = User::all();
+        return view('admin.user.index')->with('users', $users);
     }
 
     /**
